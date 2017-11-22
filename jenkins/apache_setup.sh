50cc6d3f8b0361a53c4da98568d7d5aaf0c8c6f2
Alan Lo <lo.alan@gmail.com>
2017-11-22 14:40:31 +1100

    added heat files for jenkins but not in use yet - just manual. apache setup no longer generates self signed cert.

--- jenkins/apache_setup.sh
+++ jenkins/apache_setup.sh
@@ -1,47 +1,26 @@
 #!/bin/bash
 
-set -x
+set +x
 
 # Enable apache2 SSL
-a2enmod ssl
+#a2enmod ssl
 
 # Generate Self signed certificate
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/certs/resplat.key -out /etc/ssl/certs/resplat.crt <<EOF
-AU
-Victoria
-Melbourne
-The University of Melbourne
+#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/certs/resplat.key -out /etc/ssl/certs/resplat.crt <<EOF
+#AU
+#Victoria
+#Melbourne
+#The University of Melbourne
+#
+#
+# 
+#EOF
 
 
- 
-EOF
-
-
-# Make apache2 site configuration file
-cat <<EOF > /etc/apache2/sites-available/resplat.apache2.conf
-<VirtualHost *:443>
-   SSLEngine on
-   SSLCertificateFile /etc/ssl/certs/resplat.crt
-   SSLCertificateKeyFile /etc/ssl/certs/resplat.key
-
-   Alias /static /var/www/static
-   <Directory /var/www/static>
-      Require all granted
-   </Directory>
-   <Directory /resplat/resplat>
-      <Files wsgi.py>
-         Require all granted
-      </Files>
-   </Directory>
-   WSGIDaemonProcess resplat python-home=/usr/bin/python3 python-path=/resplat
-   WSGIProcessGroup resplat
-   WSGIScriptAlias / /resplat/resplat/wsgi.py
-</VirtualHost>
-EOF
-
-
+cp jenkins/resplat.apache2.conf /etc/apache2/sites-available/
 rm /etc/apache2/sites-enabled/000-default.conf
 ln -s /etc/apache2/sites-available/resplat.apache2.conf /etc/apache2/sites-enabled/resplat.apache2.conf
 
 
 #service apache2 restart
+systemctl start apache2
