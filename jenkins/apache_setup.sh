d26211f5aa119fad7b08422970d58c0c2754f82f
Alan Lo <lo.alan@gmail.com>
2017-11-23 13:46:01 +1100

    cleanup the setup scripts

--- jenkins/apache_setup.sh
+++ jenkins/apache_setup.sh
@@ -1,26 +1,7 @@
 #!/bin/bash
 
-set +x
-
-# Enable apache2 SSL
-#a2enmod ssl
-
-# Generate Self signed certificate
-#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/certs/resplat.key -out /etc/ssl/certs/resplat.crt <<EOF
-#AU
-#Victoria
-#Melbourne
-#The University of Melbourne
-#
-#
-# 
-#EOF
-
-
 cp jenkins/resplat.apache2.conf /etc/apache2/sites-available/
 rm /etc/apache2/sites-enabled/000-default.conf
 ln -s /etc/apache2/sites-available/resplat.apache2.conf /etc/apache2/sites-enabled/resplat.apache2.conf
 
-
-#service apache2 restart
 systemctl restart apache2
--- jenkins/apt_setup.sh
+++ jenkins/apt_setup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 apt-get update
-#apt-get upgrade
+apt-get upgrade
 apt-get install -y python3-pip python3-dev python3-tk 
 apt-get install -y apache2 libapache2-mod-wsgi-py3
 apt-get install -y postgresql postgresql-contrib
 apt-get install -y git curl libpq-dev
 apt-get install -y sudo
 apt-get install -y vim
-#ln -fs /usr/bin/python3 /usr/bin/python
 pip3 install --upgrade pip
 pip install -r requirements.txt
 python3 manage.py collectstatic --no-input
