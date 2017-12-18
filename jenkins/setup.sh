6229cd3f779d144a6f734123c4a87a7ca0e9ff6b
Martin Paulo <martin.paulo@gmail.com>
2017-12-18 11:38:44 +1100

    Split the requirements.txt into two: core and test requirements

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -10,14 +10,14 @@ set -x
 
 apt-get update
 apt-get upgrade
-apt-get install -y python3-pip python3-dev python3-tk 
+apt-get install -y python3-pip python3-dev python3-tk
 apt-get install -y apache2 libapache2-mod-wsgi-py3
 apt-get install -y postgresql postgresql-contrib
 apt-get install -y git curl libpq-dev
 apt-get install -y sudo
 apt-get install -y vim
 pip3 install --upgrade pip
-pip install -r requirements.txt
+pip install -r requirements-test.txt
 python3 manage.py collectstatic --no-input
 
 cp jenkins/resplat.apache2.conf /etc/apache2/sites-available/
